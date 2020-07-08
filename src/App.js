a132f8be7da967d5c0afc4c68deb3ff7a4182405
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-07-08 16:08:15 +0300

    08.07.2020

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import PromoContent from "./components/PromoContent"
+// import PromoContent from "./components/PromoContent"
+import Header from "./components/Header"
 import Home from './Pages/Home';
 import Gallery from './Pages/Gallery';
 import About from './Pages/About';
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className='wrapper'>
       <BrowserRouter>
+        <Header/>
         <Route exact path='/gallery' component={Gallery} />
         <Route exact path='/about' component={About} />
         <Route exact path='/contact' component={Contact} />
--- src/components/PromoContent.js
+++ src/components/PromoContent.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from "./Header"
 
 const PromoContent =(props)=>{
     return (
         <div className='promoContent'>
-            <Header/>
             <h1>{props.headText}</h1>
         </div>
       );
