f037b6391d0b08f53835a07298a84193b851ebbb
vvalik <ghost0962421083>
2020-07-09 12:01:22 +0300

    09.07.2020 info

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ import {InfoContext} from "../App";
 
 
 const Home =()=>{
-     const text ={
+     const infoText ={
          first: {
              left: {
                  title: 'SERVICES',
@@ -15,15 +15,58 @@ const Home =()=>{
                  title: 'Fermentum nulla bibendum pretium facilisis elit imperdiet. Sed cras dolor nisi quam at maecenas ut.',
                  text: 'lorem lorem lorem lorem'
              }
+         },
+         secondary:{
+             title: "TECHNOLOGIES",
+             tiles:[
+                 {
+                     img: './assets/img/techologies/web/js.svg',
+                     title:'HTML 5',
+                     text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque sed purus cras risus eget amet interdum faucibus tincidunt.'
+                 },
+                 {
+                     img: './assets/img/techologies/web/js.svg',
+                     title:'CSS 3',
+                     text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque sed purus cras risus eget amet interdum faucibus tincidunt.'
+                 },
+                 {
+                     img: './assets/img/techologies/web/js.svg',
+                     title:'JAVA SCRIPT',
+                     text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque sed purus cras risus eget amet interdum faucibus tincidunt.'
+                 },
+                 {
+                     img: './assets/img/techologies/web/js.svg',
+                     title:'JQUERY',
+                     text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque sed purus cras risus eget amet interdum faucibus tincidunt.'
+                 },
+                 {
+                     img: './assets/img/techologies/web/js.svg',
+                     title:'FIGMA',
+                     text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque sed purus cras risus eget amet interdum faucibus tincidunt.'
+                 },
+                 {
+                     img: './assets/img/techologies/web/js.svg',
+                     title:'PHOTOSHOP',
+                     text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque sed purus cras risus eget amet interdum faucibus tincidunt.'
+                 },
+                 {
+                     img: './assets/img/techologies/web/js.svg',
+                     title:'ILLUSTRATOR',
+                     text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque sed purus cras risus eget amet interdum faucibus tincidunt.'
+                 },
+                 {
+                     img: './assets/img/techologies/web/js.svg',
+                     title:'AFTER EFFECTS',
+                     text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque sed purus cras risus eget amet interdum faucibus tincidunt.'
+                 }
+             ]
          }
      }
     return (
     <>
     <PromoContent headText={`GAME\nLIKE\n LIFE,\nIS\nNOT\nPREDICTIBLE`}/>
 
-    <InfoContext.Provider
-        value ={{text}}
-    >
+    <InfoContext.Provider value ={{infoText}}>
         <Info/>
     </InfoContext.Provider>
 </>
--- src/components/InfoFirst.js
+++ src/components/InfoFirst.js
@@ -13,23 +13,39 @@ const InfoFirst = () => {
 }
 export default InfoFirst
 
-const InfoFirstComponent = (props) => {
+const InfoFirstComponent = ({infoText}) => {
     return (
         <div className={"info_bg"}>
             <div className="container">
                 <div className="info_first">
                     <div className={"info_first_left"}>
-                        <h2 className={"info_first_left_h2"}>{props.text.first.left.title}</h2>
+                        <h2 className={"info_first_left_h2"}>{infoText.first.left.title}</h2>
                         <ul className={'info_first_left_ul'}>
-                            {props.text.first.left.list.map(value => {
+                            {infoText.first.left.list.map(value => {
                                 return <li>{value}</li>
                             })}
                         </ul>
                     </div>
                     <div className={'info_first_right'}>
-                        <h2 className={'info_first_right_h2'}>{props.text.first.right.title}</h2>
-                        <p>{props.text.first.right.text}</p>
+                        <h2 className={'info_first_right_h2'}>{infoText.first.right.title}</h2>
+                        <p>{infoText.first.right.text}</p>
                     </div>
+                </div>
+                <div className="info_secondary">
+                    <h3>{infoText.secondary.title}</h3>
+                    <ul>
+                        {
+                            infoText.secondary.tiles.map(value =>{
+                                return (
+                                    <li>
+                                        <img src={value.img} alt={value.title}/>
+                                        <h5>{value.title}</h5>
+                                        <p>{value.text}</p>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
                 </div>
             </div>
         </div>
