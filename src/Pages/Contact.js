ecc8fd9de895a9dddf2917c803782641d445b794
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-07-09 12:00:50 +0300

    09.07.2020

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PromoContent from '../components/PromoContent';
+import Images from "../assets/promoImg/promo4.png"; 
 
 const Contact =()=> {
     return (
-      <PromoContent headText={`RECORDING\nTHE\nSOUND OF\nSOUL`}/>
+      <PromoContent headText={`RECORDING\nTHE\nSOUND OF\nSOUL`} promoSrc={Images}/>
     );
 }
 export default Contact
--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PromoContent from '../components/PromoContent';
+import Images from "../assets/promoImg/promo2.png"; 
 
 const Gallery =()=> {
     return (
-      <PromoContent headText={`RECOR\nDING THE\nSOUND OF\nSOUL`}/>
+      <PromoContent headText={`RECOR\nDING THE\nSOUND OF\nSOUL`} promoSrc={Images}/>
     );
 }
 export default Gallery
