8a0f56aff3f47925f6e0c0a68a05061ca9a3bf12
DimaKyrdiashkin <44475715+DimaKyrdiashkin@users.noreply.github.com>
2020-07-09 09:32:51 +0300

    09.07.2020

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import {  NavLink } from 'react-router-dom';
 const NavBar =()=>{
     return(
         <nav>
-            <NavLink exact={true} to='/'>Home</NavLink>
+            <NavLink exact to='/'>Home</NavLink>
             <NavLink to='/gallery'>Gallery</NavLink>
             <NavLink to='/about'>About us</NavLink>
             <NavLink to='/contact'>Contact</NavLink>
