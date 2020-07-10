462db2ecf492b050a472e793c2b537e9208d31a7
vvalik <ghost0962421083>
2020-07-10 13:04:36 +0300

    09.07.2020 info

--- src/components/InfoFirst.jsx
+++ src/components/InfoFirst.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import {InfoContext} from "../App";
 
 
@@ -14,21 +14,55 @@ const InfoFirst = () => {
 export default InfoFirst
 
 const InfoFirstComponent = ({infoText}) => {
+
+    const [text, tabl] = useState(infoText.first.title.text);
+    const [title, tablTitle] = useState(infoText.first.title.titleH2);
+    const [liActiv, tablLiActiv] = useState(null)
+
+
+    useEffect(()=>{
+        const funcTions = ()=>{
+            tablLiActiv(null)
+        }
+
+    })
+    console.log(infoText)
+
+
+
+
     return (
         <div className={"info_bg"}>
             <div className="container">
                 <div className="info_first">
                     <div className={"info_first_left"}>
-                        <h3 className={"info_first_left_h2"}>{infoText.first.left.title}</h3>
+                        <h3
+                            className={"info_first_left_h2"}
+                            onClick={()=>{
+                                tabl(infoText.first.title.text)
+                                tablTitle(infoText.first.title.titleH2)
+                            }}
+                        >
+                            {infoText.first.title.title}
+                        </h3>
                         <ul className={'info_first_left_ul'}>
-                            {infoText.first.left.list.map((value) => {
-                                return <li key={value.toString()}>{value}</li>
+                            {infoText.first.tiles.map((value, index) => {
+                                return <li
+                                    className={liActiv}
+                                    key={index}
+                                    onClick={()=>{
+                                        tabl(value.text)
+                                        tablTitle("")
+                                    }}
+                                >
+                                    {value.title}
+                                </li>
                             })}
                         </ul>
                     </div>
                     <div className={'info_first_right'}>
-                        <h2 className={'info_first_right_h2'}>{infoText.first.right.title}</h2>
-                        <p>{infoText.first.right.text}</p>
+                        <h2 className={'info_first_right_h2'}>{title}</h2>
+                        <p>{text}</p>
                     </div>
                 </div>
             </div>
--- src/components/InfoSecond.jsx
+++ src/components/InfoSecond.jsx
@@ -1,33 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import {InfoContext} from "../App";
 
 const InfoSecond = () => {
     return (
         <InfoContext.Consumer>
             {value => (<InfoSecondComponent {...value}/>)}
-
         </InfoContext.Consumer>
     )
 
 }
 export default InfoSecond
+
+
 const InfoSecondComponent = ({infoText}) => {
-    return(
+
+    return (
         <div className="container">
             <div className="info_second">
                 <h2 className={"info_second_h2"}>{infoText.second.title}</h2>
                 <ul className={"info_second_ul"}>
                     {
-                        infoText.second.tiles.map((value, index) =>
-                            <li key={index} className={"info_second_li"}>
-                                <img src={value.img} alt={value.title}/>
-                                <h5>{value.title}</h5>
-                                <p>{value.text}</p>
-                            </li>
-                        )
+                        infoText.second.tiles.map((value, index) =><Item value={value} key={index}/>)
                     }
                 </ul>
             </div>
         </div>
     )
+}
+
+const Item = (props)=>{
+    const [color, setColor] = useState("linear-gradient(135.07deg, #0C0C0C 0%, rgba(12, 12, 12, 0) 100%");
+    return(
+        <li
+            onMouseOver={()=>setColor(props.value.color)}
+            onMouseOut={()=>setColor("linear-gradient(135.07deg, #0C0C0C 0%, rgba(12, 12, 12, 0) 100%")}
+            className={"info_second_li"}
+            style={{background:color}}
+        >
+            <img
+                src={props.value.img}
+                alt={props.value.title}
+            />
+            <h5>{props.value.title}</h5>
+            <p>{props.value.text}</p>
+        </li>
+    )
+
 }
