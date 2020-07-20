3b84a06e79bc9b4a037ff992fa55ecd6ebcd7c08
vvalik <ghost0962421083>
2020-07-20 15:14:13 +0300

    20.07.2020

---
+++ src/Pages/ProjectName/ProjectName.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectName = () => {
+    return (
+        <>
+            <section>
+
+            </section>
+        </>
+    )
+}
+
+export default ProjectName
